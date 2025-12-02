8393dbb7026d8bb4cd69e84fba8d093f4132de7a
choi <cejewe@naver.com>
2025-12-03 02:47:04 +0900

    23: DB 성능 향상

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,23 @@
 import { PrismaClient } from "./generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = globalThis.prisma || new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
+const adapter = new PrismaNeon({ connectionString });
+
+export const prisma =
+  globalThis.prisma ||
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = prisma;
